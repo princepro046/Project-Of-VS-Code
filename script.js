f0bc9dfe040eb95033c333cf61103b7d79e17dee
princepro046 <princepro046@gmail.com>
2024-09-03 20:27:32 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// function greeter(){
+//      var greeting;
+//      var time = new Date().getHours();
+//      if (time < 10){
+//         //0-10
+//         greeting = "Good Morning";
+//      }
+    
+//      else if (time < 20) {
+//         //10-20
+//         greeting = "Good Day";
+
+//      }
+//      else {
+//              greeting= "Good Evening";
+
+//      }
+// document.getElementById("hour").innerHTML = "Current Hour is :" + time;
+// document.getElementById("greet").innerHTML = greeting;
+
+
+// Multiplacation Of 20
+var i=1;
+var num = 20;
+document.write("Multiplacation Table Of" +num + "<br>")
+while(i<=10){
+       //20*1=20
+       //20*2=40
+        document.write(num + "x" + i + "=" + num*i + "<br>")
+        i++;
+}
+
+
+
+     
